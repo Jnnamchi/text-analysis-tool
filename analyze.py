c9fc85058ef64f03160afcf2bc03df7adbc46f1f
John Nnamchi <john.nnamchi@mail.mcgill>
2021-01-27 21:52:51 -0500

    Added word and sentence tokenization

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 from random_username.generate import generate_username
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 # Welcome User
 def welcomeUser():
@@ -42,9 +43,27 @@ def getArticleText():
 	f.close()
 	return rawText.replace("\n", " ").replace("\r", "")
 
+# Extract Sentences from raw Text Body
+def tokenizeSentences(rawText):
+	return sent_tokenize(rawText)
+
+# Extract Words from list of Sentences
+def tokenizeWords(sentences):
+	words = []
+	for sentence in sentences:
+		words.extend(word_tokenize(sentence))
+	return words
+
+# Get User Details
 welcomeUser()
 username = getUsername()
 greetUser(username)
+
+# Extract and Tokenize Text
 articleTextRaw = getArticleText()
+articleSentences = tokenizeSentences(articleTextRaw)
+articleWords = tokenizeWords(articleSentences)
+
+# Print for testing
 print("GOT:")
-print(articleTextRaw)
+print(articleWords)
