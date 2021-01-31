58a6291d3b638a581855c046f724a221d982a106
John Nnamchi <john.nnamchi@mail.mcgill>
2021-01-31 16:45:13 -0500

    Added avg words per sentence calculation

--- analyze.py
+++ analyze.py
@@ -64,6 +64,13 @@ def extractKeySentences(sentences, searchPattern):
 			matchedSentences.append(sentence)
 	return matchedSentences
 
+# Get the average words per sentence, excluding punctuation
+def getWordsPerSentence(sentences):
+	totalWords = 0
+	for sentence in sentences:
+		totalWords += len(sentence.split(" "))
+	return totalWords / len(sentences)
+
 # Get User Details
 welcomeUser()
 username = getUsername()
@@ -77,7 +84,8 @@ articleWords = tokenizeWords(articleSentences)
 # Get Analytics
 stockSearchPattern = "[0-9]|[%$€£]|thousand|million|billion|trillion|profit|loss"
 keySentences = extractKeySentences(articleSentences, stockSearchPattern)
+wordsPerSentence = getWordsPerSentence(articleSentences)
 
 # Print for testing
 print("GOT:")
-print(keySentences)
+print(wordsPerSentence)
