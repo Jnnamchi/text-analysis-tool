1bedb90a43294f5585d5970465aeaaf64cf0754a
John Nnamchi <jnnamchi@Johns-MacBook-Pro.local>
2024-03-24 17:33:57 -0700

    Integrated text analyzer into stock analysis

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -1,7 +1,9 @@
+import json
 import yfinance as yf
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
+import analyze
 
 def extractBasicInfo(data):
 	keysToExtract = [ 'longName', 'website', 'sector', 'fullTimeEmployees', 'marketCap', 'totalRevenue', 'trailingEps' ]
@@ -71,6 +73,10 @@ def getCompanyStockInfo(tickerSymbol):
 	futureEarningsDates = getEarningsDates(company)
 	newsArticles = getCompanyNews(company)
 	newsArticlesAllText = extractCompanyNewsArticles(newsArticles)
-	print(newsArticlesAllText)
+	newsTextAnalysis = analyze.analyzeText(newsArticlesAllText)
+
+	finalResultJson = json.dumps(newsTextAnalysis, indent=4)
+	# Print for testing
+	print(finalResultJson)
 
 getCompanyStockInfo('MSFT')
