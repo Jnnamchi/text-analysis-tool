5026765c567c5dd5dd13b9e781a5b287d98e15d7
John Nnamchi <jnnamchi@Johns-MacBook-Pro.local>
2024-04-17 18:38:55 -0700

    Completed stock analyzer

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -75,8 +75,14 @@ def getCompanyStockInfo(tickerSymbol):
 	newsArticlesAllText = extractCompanyNewsArticles(newsArticles)
 	newsTextAnalysis = analyze.analyzeText(newsArticlesAllText)
 
-	finalResultJson = json.dumps(newsTextAnalysis, indent=4)
-	# Print for testing
-	print(finalResultJson)
+	finalStockAnalysis = {
+		"basicInfo": basicInfo,
+		"priceHistory": priceHistory,
+		"futureEarningsDates": futureEarningsDates,
+		"newsArticles": newsArticles,
+		"newsTextAnalysis": newsTextAnalysis
+	}
+	return finalStockAnalysis
 
-getCompanyStockInfo('MSFT')
+# companyStockAnalysis = getCompanyStockInfo('MSFT')
+# print(json.dumps(companyStockAnalysis, indent=4))
