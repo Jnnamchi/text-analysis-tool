0151aa18052105a1b098d8f02337f2932f20fbba
Jnnamchi <john.nnamchi@mail.mcgill.ca>
2024-12-08 16:17:00 -0800

    Re-added company analysis workflow

--- server.py
+++ server.py
@@ -23,15 +23,17 @@ def healthCheck():
 
 @app.route('/analyze-stock/<ticker>', methods=["GET"])
 def analyzeStock(ticker):
-    return stockDataTest
-    # if len(ticker) > 5 or not ticker.isidentifier():
-    #     abort(400, 'Invalid ticker symbol')
-    # try:
-    analysis = getCompanyStockInfo(ticker)
-    # except NameError as e:
-    #     abort(404, e)
-    # except:
-    #     abort(500, 'Something went wrong running the stock analysis.')
+    # return stockDataTest
+    if len(ticker) > 5 or not ticker.isidentifier():
+        abort(400, 'Invalid ticker symbol')
+    try:
+        analysis = getCompanyStockInfo(ticker)
+    except NameError as e:
+        abort(404, e)
+    except e:
+        print('Something went wrong running the stock analysis:')
+        print(e)
+        abort(500, 'Something went wrong running the stock analysis.')
     return analysis
 
 @app.route('/analyze-text', methods=["POST"])
