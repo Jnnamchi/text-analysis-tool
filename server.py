ab317d11f0729747544ecf0ca82f8211a5697212
Jnnamchi <john.nnamchi@mail.mcgill.ca>
2024-12-17 14:27:42 -0800

    Put back error printing

--- server.py
+++ server.py
@@ -24,15 +24,15 @@ def healthCheck():
 @app.route('/analyze-stock/<ticker>', methods=["GET"])
 def analyzeStock(ticker):
     # return stockDataTest
-    if len(ticker) > 5 or not ticker.isidentifier():
-        abort(400, 'Invalid ticker symbol')
-    try:
-        analysis = getCompanyStockInfo(ticker)
-    except NameError as e:
-        abort(404, e)
-    except Exception as e:
-        print(f"Error running the stock analysis: {e}")
-        abort(500, 'Something went wrong running the stock analysis.')
+    # if len(ticker) > 5 or not ticker.isidentifier():
+    #     abort(400, 'Invalid ticker symbol')
+    # try:
+    analysis = getCompanyStockInfo(ticker)
+    # except NameError as e:
+    #     abort(404, e)
+    # except Exception as e:
+    #     print(f"Error running the stock analysis: {e}")
+    #     abort(500, 'Something went wrong running the stock analysis.')
     return analysis
 
 @app.route('/analyze-text', methods=["POST"])
